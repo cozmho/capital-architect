2935ff06736ea81fdba75d18c8ea480a2d62788d
Capital Architect <admin@capitalarchitect.tech>
2026-04-13 02:30:05 -0500

    Configure @prisma/adapter-pg for Vercel without TS errors

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,11 +1,21 @@
 import { PrismaClient } from "@prisma/client";
+import { Pool } from "pg";
+import { PrismaPg } from "@prisma/adapter-pg";
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-export const prisma =
-  globalForPrisma.prisma ||
-  new PrismaClient({
-    log: ["query"],
-  });
+let prismaClient: PrismaClient;
+
+if (globalForPrisma.prisma) {
+  prismaClient = globalForPrisma.prisma;
+} else {
+  // Use connection pooling to interact with Supabase natively on Vercel
+  const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+  const adapter = new PrismaPg(pool);
+  
+  prismaClient = new PrismaClient({ adapter, log: ["query"] });
+}
+
+export const prisma = prismaClient;
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
